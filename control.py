60cc10b82f894ac133fcd674b19ced3be5d9ccfa
Adriana Mundo <adrianamundog@hotmail.com>
2019-10-21 23:45:42 -0600

    README actualizado y agregado Dockerfile

--- control.py
+++ control.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, render_template, request
+from flask_caching import Cache
 import os, optparse, sys
 import json
 import requests
@@ -15,9 +16,26 @@ tmdb.api_key = '9083cbdd7f380052de7a54baf7d4983b'
 movie = Movie()
 popular = movie.popular()
 
+for p in popular:
+    title = str(p.title)
+    release = str(p.release_date)
+    popularity = str(p.popularity)
+
+    #print(len(p.title))
+    #print(len(p.release_date))
+    #print(p.popularity)
+
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+cache.init_app(app)
+
+filename = os.path.join(app.static_folder, 'jsons', 'trending_movies.json')
+with open(filename) as test_file:
+    data = json.load(test_file)
+
 @app.route("/")
+@cache.cached(timeout= 50)
 def about():
-    return render_template("index.html", info=info)
+    return render_template("index.html", data=data)
 
 
 if __name__ == "__main__":
