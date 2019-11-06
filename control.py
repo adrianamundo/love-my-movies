b87fc4d991d8755e489fdb49390cd34db807310e
Adriana Mundo <adrianamundog@hotmail.com>
2019-11-06 17:12:02 -0600

    api in function

--- control.py
+++ control.py
@@ -9,28 +9,20 @@ from tmdbv3api import Movie
 app = Flask(__name__)
 
 environment=os.getenv("ENVIRONMENT","development")
-info = {}
+
 tmdb = TMDb()
 tmdb.api_key = '9083cbdd7f380052de7a54baf7d4983b'
 
-movie = Movie()
-popular = movie.popular()
+response = requests.get('https://api.themoviedb.org/3/trending/movie/week?api_key=9083cbdd7f380052de7a54baf7d4983b')
+data = response.json()
+with open('Trending_Movies_API.json', 'w', encoding= 'utf-8') as f:
+    json.dump(data, f, ensure_ascii= False, indent=4)
 
-for p in popular:
-    title = str(p.title)
-    release = str(p.release_date)
-    popularity = str(p.popularity)
-
-    #print(len(p.title))
-    #print(len(p.release_date))
-    #print(p.popularity)
 
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 cache.init_app(app)
 
-filename = os.path.join(app.static_folder, 'jsons', 'trending_movies.json')
-with open(filename) as test_file:
-    data = json.load(test_file)
+
 
 @app.route("/")
 @cache.cached(timeout= 50)
