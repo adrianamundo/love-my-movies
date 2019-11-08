53eb3873843374dc99876a1662967f4e836fb3cf
Adriana Mundo <adrianamundog@hotmail.com>
2019-11-07 23:34:02 -0600

    Added travis file

--- control.py
+++ control.py
@@ -5,7 +5,7 @@ import json
 import requests
 from tmdbv3api import TMDb
 from tmdbv3api import Movie
-import redis 
+from redis import Redis
 
 app = Flask(__name__)
 
@@ -18,6 +18,44 @@ response = requests.get('https://api.themoviedb.org/3/trending/movie/week?api_ke
 data = response.json()
 with open('Trending_Movies_API.json', 'w', encoding= 'utf-8') as f:
     json.dump(data, f, ensure_ascii= False, indent=4)
+
+class db:
+    key = "9083cbdd7f380052de7a54baf7d4983b"
+    redisserver = NONE
+    environment = ""
+
+    def __init__(self, key, environment):
+        self.key = key
+        self.environment = environment
+    
+    def redisinit(self):
+        movie = Movie()
+        if self.environment == "development":
+            self.environment = "localhost"
+        
+        self.redisserver = redis.StrictRedis(host= self.environment, port=6379, db=0)
+        apisource = TMDb()
+        apisource.key = self.key
+        self.Popularmovies(movie)
+
+        def Popularmovies(self,movie):
+            popular = movie.popular()
+            self.saveMovie(popular)
+        
+        def saveMovie(self, moviesbyeone):
+            with self.redisserver.pipeline() as pipe:
+                for moviesbyone in moviesbyeone:
+                    pipe.hset(moviesbyeone.title, "title", moviesbyeone.title)
+                    pipe.hset(moviesbyone.title, "release_date", moviesbyeone.title)
+                    pipe.hset(moviesbyone.title, "popularity", moviesbyeone.title)
+                    pipe.execute()
+
+database = database(key, environment)
+database.redisinit()
+redisserver=database.redisserver
+
+
+
 
 
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
